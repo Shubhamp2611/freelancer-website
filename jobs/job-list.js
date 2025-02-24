8003f8ebf680650a265a68156a7c52522ddfd17b
Shubham patil <shree22kar2411@gmail.com>
2025-02-24 17:45:18 +0530

    added new page

---
+++ jobs/job-list.js
@@ -0,0 +1,26 @@
+const jCategory = [
+
+
+    {
+        index: 0,
+        image: "../images/google.png",
+        title: "Web Developer",
+        rate: "$900-1200/m",
+        av: "Full time",
+        comapanyName: "Google",
+        location: "USA",
+        vacancy: "01",
+        hours: "50h / week",
+        description: "We are seeking a skilled Web Developer to design, develop, and maintain responsive and dynamic websites and web applications. The ideal candidate should have strong expertise in front-end and/or back-end development, with proficiency in technologies such as HTML, CSS, JavaScript, and frameworks like Bootstrap or Tailwind CSS. Experience with frontend libraries like React, Angular, or Vue, along with backend technologies such as Node.js, Express.js, or Java with Spring Boot, is a plus. The role involves writing clean, efficient, and well-documented code, optimizing applications for performance and security, and ensuring cross-browser compatibility. The candidate will collaborate with designers, developers, and project managers to deliver high-quality web solutions while staying updated with the latest web development trends. Debugging and troubleshooting website issues, as well as maintaining scalability and responsiveness, will also be key responsibilities in this role.",
+        workpalce: "work for home",
+        eduaction: "Bechelor degree in any Computer Science Course.",
+        exprience: "2 to 5 year(s)"
+    },
+    {
+        index: 1,
+        image: "../images/uber.png",
+        title: "Business Associate",
+        rate: "$900-1200/m",
+        av: "Part-time",
+    },
+];
---
+++ jobs/jobsearch.js
@@ -0,0 +1,36 @@
+const categories = Array.from(jCategory);
+
+document.getElementById("searchbar").
+addEventListener("keyup", (e) => {
+    const searchData = e.target.value.toLowerCase()
+    const filterData = categories.filter((item) =>
+        item.title.toLowerCase().includes(searchData)
+    );
+    displayItems(filterData);
+});
+
+const displayItems = (items) => {
+    const rootElement = document.getElementById
+    ("root");
+    rootElement.innerHTML = "";
+
+    items.forEach((item) => {
+        const {index, image, title, rate, av} = item;
+        const jList = document.createElement("div");
+        jList.className = "jList";
+        jList.innerHTML = `
+        <img src="${image}" alt="">
+        <h3>${title}</h3>
+        <p>${rate}/m</p>
+        <span id="key">${av}</span>
+        `;
+
+        rootElement.appendChild(jList);
+
+        jList.addEventListener("click", ()=>{
+            window.location.href = 'job-detail.html?id=${index}';
+        });
+    });
+};
+
+displayItems(categories);
